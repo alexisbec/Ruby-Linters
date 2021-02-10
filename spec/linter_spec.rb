1e89d30f2721a7c33cf0aac512bb30a8006ef4cd
Alexis Varela <alexisbec13@hotmail.com>
2021-02-10 09:51:21 -0600

    fixed linter errors

--- spec/linter_spec.rb
+++ spec/linter_spec.rb
@@ -4,14 +4,14 @@ describe Linter do
   describe '.two_spaces' do
     it 'return an error if there are two spaces between elements' do
       line = 'def  method'
-      expect(Linter.two_spaces('example.rb', line, 1)) == ("example.rb line: 2 'Two spaces between elements'")
+      expect(Linter.two_spaces('example.rb', line, 1)) == "example.rb line: 2 'Two spaces between elements'"
     end
   end
 
   describe '.ending_spaces' do
     it 'return an error if the line has a trailing space' do
       expect(Linter.ending_spaces('checks.rb', 'def method ',
-                                  5)) == ("checks.rb line 6: 'Ending blank space(s) in the line.'")
+                                  5)) == "checks.rb line 6: 'Ending blank space(s) in the line.'"
     end
   end
 
@@ -23,8 +23,8 @@ describe Linter do
 
   describe '.between_ope1' do
     it 'return an error if there is an space between operators' do
-      expect(Linter.between_ope_1('checks.rb', 'if counter = = 1',
-                                  7)) == ("checks.rb line: 8 'Space between two operators.'")
+      expect(Linter.between_ope1('checks.rb', 'if counter = = 1',
+                                  7)) == "checks.rb line: 8 'Space between two operators.'"
     end
   end
 
@@ -33,7 +33,7 @@ describe Linter do
       line = 'if counter =1'
       file = 'example.rb'
       index = 3
-      expect(Linter.after_ope(file, line, index)) == ("example.rb line: 4 'Missing space after operator.'")
+      expect(Linter.after_ope(file, line, index)) == "example.rb line: 4 'Missing space after operator.'"
     end
   end
 
@@ -42,7 +42,7 @@ describe Linter do
       line = 'if counter= 1'
       file = 'example.rb'
       index = 4
-      expect(Linter.after_ope(file, line, index)) == ("example.rb line: 5 'Missing space before operator.'")
+      expect(Linter.after_ope(file, line, index)) == "example.rb line: 5 'Missing space before operator.'"
     end
   end
 
@@ -50,7 +50,7 @@ describe Linter do
     context 'there are no ending semicolons'
 
     it 'return an error if there is an ending semicolon' do
-      expect(Linter.ending_semicolon('checks.rb', 'def method', 1)) == (nil)
+      expect(Linter.ending_semicolon('checks.rb', 'def method', 1)) == nil
     end
   end
 end
